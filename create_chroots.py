82835329902a5eb902a472b3070d684dc738fb91
kux <alexandruioan.cucu@gmail.com>
2015-07-26 01:30:01 +0300

    Refactored the code by creating Nameserver and Zone classes which would allow for easier extension

--- create_chroots.py
+++ create_chroots.py
@@ -8,6 +8,84 @@ import time
 import jinja2
 
 
+ENV = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
+
+class Nameserver(object):
+    def __init__(self, name, ip, use_chroot):
+        self.name = name
+        self.ip = ip
+        self.use_chroot = use_chroot
+        self.zones = []
+
+    @property
+    def base_dir(self):
+        if self.use_chroot:
+            return ''
+        return os.path.join(os.getcwd(), 'chroots', self.name)
+
+    def render_named9_conf(self):
+        named9_conf_template = ENV.get_template('named9.conf')
+        return named9_conf_template.render(ns=self)
+
+    def build_dirs(self):
+        os.makedirs(os.path.join('chroots', self.name))
+        os.makedirs(os.path.join('chroots', self.name, 'var/named/zones'))
+        os.makedirs(os.path.join('chroots', self.name, 'var/log'))
+
+    def build_chroot(self):
+        logging.info('Building %s', self.name)
+        self.build_dirs()
+        with open('chroots/%s/var/named/named9.conf' % self.name, 'w') as fh:
+            fh.write(self.render_named9_conf())
+        for zone in self.zones:
+            zone.write_zonefile(self.name)
+
+
+class MasterZone(object):
+    def __init__(self, name, auth_nameservers,
+                 refresh, retry, expire, negative_ttl,
+                 record_count):
+        self.name = name
+        self.auth_nameservers = auth_nameservers
+        self.refresh = refresh
+        self.retry = retry
+        self.expire = expire
+        self.negative_ttl = negative_ttl
+        self.test_records = ['test%d' % i for i in xrange(record_count)]
+
+    @property
+    def type(self):
+        return 'master'
+
+    @property
+    def is_slave(self):
+        return False
+
+    def write_zonefile(self, ns_name):
+        zone_template = ENV.get_template('zone_template')
+        file_path = 'chroots/%s/var/named/zones/%s.zone' % (ns_name, self.name)
+        with open(file_path, 'w') as fh:
+            fh.write(zone_template.render(zone=self))
+
+
+class SlaveZone(object):
+    def __init__(self, name, master_ips):
+        self.name = name
+        self.master_ips = master_ips
+
+    @property
+    def type(self):
+        return 'slave'
+
+    @property
+    def is_slave(self):
+        return True
+
+    def write_zonefile(self, ns_name):
+        pass
+
+
 def run_command(command, stop_on_failure=True):
     logging.debug('Running command: %s', command)
     command = command.split()
@@ -33,63 +111,6 @@ def clean_existing_directories():
     logging.info('Removing existing chroots')
     if os.path.exists('chroots'):
         shutil.rmtree('chroots')
-
-
-def build_dirs(chroot_name):
-    os.makedirs(os.path.join('chroots', chroot_name))
-    os.makedirs(os.path.join('chroots', chroot_name, 'var/named/zones'))
-    os.makedirs(os.path.join('chroots', chroot_name, 'var/log'))
-
-
-def build_chroot(env, args, chroot_dir, ns_ip, master_ips=None):
-    logging.info('Building %s', chroot_dir)
-    build_dirs(chroot_dir)
-    ns_type = 'master' if master_ips is None else 'slave'
-    if args.no_chroots:
-        base_dir = os.path.join(os.getcwd(), 'chroots', chroot_dir)
-    else:
-        base_dir = ''
-    named9_conf_template = env.get_template('named9.conf')
-    zones = ['zone%d.com' % i for i in xrange(args.zones)]
-    with open('chroots/%s/var/named/named9.conf' % chroot_dir, 'w') as fh:
-        fh.write(named9_conf_template.render(
-            ns_type=ns_type,
-            zones=zones,
-            ns_ip=ns_ip,
-            master_ips=master_ips,
-            base_dir=base_dir
-        ))
-    if ns_type == 'master':
-        zone_template = env.get_template('zone_template')
-        for zone_name in zones:
-            path = 'chroots/master/var/named/zones/%s.zone'
-            with open(path % zone_name, 'w') as fh:
-                fh.write(zone_template.render(
-                    zone_name=zone_name,
-                    refresh=args.refresh,
-                    retry=args.retry,
-                    expire=args.expire,
-                    negative_ttl=args.negative_ttl,
-                    master_ip=args.master_ip,
-                    xfr_ips=ns_ips(args.xfr_ip_prefix, args.xfr_count),
-                    resolver_ips=ns_ips(args.resolver_ip_prefix,
-                                        args.resolver_count),
-                    test_records=xrange(args.records_count)
-                ))
-
-
-def build_all_chroots(env, args):
-    build_chroot(env, args, 'master', args.master_ip)
-    xfr_ips = ns_ips(args.xfr_ip_prefix, args.xfr_count)
-    resolver_ips = ns_ips(args.resolver_ip_prefix, args.resolver_count)
-    for index, ns_ip in enumerate(xfr_ips):
-        build_chroot(env, args, 'xfr%d' % index, ns_ip, [args.master_ip])
-    for index, ns_ip in enumerate(resolver_ips):
-        build_chroot(
-            env, args, 'resolver%d' % index,
-            ns_ip,
-            ns_ips(args.xfr_ip_prefix, args.xfr_count)
-        )
 
 
 def kill_running_nameservers():
@@ -118,51 +139,48 @@ def start_nameservers(ns_path, use_chroots):
             run_command('%s -c %s' % (ns_path, conf_path))
 
 
-def nsupdate_loop(env, args):
+def nsupdate_loop(nsupdate_path, update_interval,
+                  master_ns, zones):
     current_value = 1
-    if args.updated_zones == 'all':
-        zones = xrange(args.zones)
-    else:
-        zones = [int(zone) for zone in args.updated_zones.split(',')]
 
     while True:
-        nsupdate_template = env.get_template('nsupdate')
+        nsupdate_template = ENV.get_template('nsupdate')
         logging.info('Updating test TXT record from %d to %d',
                      current_value, current_value + 1)
-        for zone_no in zones:
+        for zone in zones:
             nsupdate_statements = nsupdate_template.render(
-                master_ip=args.master_ip,
+                master_ip=master_ns.ip,
                 old_value=current_value,
                 new_value=current_value + 1,
-                zone_no=zone_no
+                zone=zone
             ).replace('\n\n', '\n')
             with open('/tmp/nsupdate_statements', 'w') as fh:
                 fh.write(nsupdate_statements)
-            run_command('%s /tmp/nsupdate_statements' % args.nsupdate_path)
+            run_command('%s /tmp/nsupdate_statements' % nsupdate_path)
         current_value += 1
-        time.sleep(args.nsupdate_interval)
+        time.sleep(update_interval)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Generate bind9 chroots')
-    parser.add_argument('--zones', type=int, default=2)
+    parser.add_argument('--zones-count', type=int, default=2)
     parser.add_argument('--master-ip', default='127.1.1.1')
     parser.add_argument('--xfr-ip-prefix', default='127.2.2')
     parser.add_argument('--resolver-ip-prefix', default='127.3.3')
 
     parser.add_argument('--xfr-count', type=int, default=2)
     parser.add_argument('--resolver-count', type=int, default=1)
+    parser.add_argument('--subdomain-resolver-count', type=int, default=0)
 
     parser.add_argument('--refresh', type=int, default=60)
     parser.add_argument('--retry', type=int, default=30)
     parser.add_argument('--expire', type=int, default=300)
     parser.add_argument('--negative_ttl', type=int, default=5)
-    parser.add_argument('--records-count', type=int, default=1000)
+    parser.add_argument('--record-count', type=int, default=1000)
 
     parser.add_argument('--ns-path')
 
     parser.add_argument('--nsupdate-path')
-    parser.add_argument('--updated-zones', default='all')
     parser.add_argument('--nsupdate-interval', type=int, default=1)
 
     parser.add_argument('--no-chroots', action='store_true', default=False)
@@ -175,15 +193,58 @@ def main():
         level=logging.DEBUG if args.debug else logging.INFO
     )
 
-    if args.zones < 1:
+    use_chroot = not args.no_chroots
+
+    master_ns = Nameserver(
+        name='master',
+        ip=args.master_ip,
+        use_chroot=use_chroot)
+    xfrs = [
+        Nameserver(
+            name='xfr%d' % i,
+            ip='%s.%d' % (args.xfr_ip_prefix, i + 1),
+            use_chroot=use_chroot)
+        for i in xrange(args.xfr_count)]
+    resolvers = [
+        Nameserver(
+            name='resolver%d' % i,
+            ip='%s.%d' % (args.resolver_ip_prefix, i + 1),
+            use_chroot=use_chroot)
+        for i in xrange(args.resolver_count)]
+    auth_nameservers = [master_ns] + xfrs + resolvers
+
+    master_zones = [
+        MasterZone('zone%d.com' % i,
+                   auth_nameservers=auth_nameservers,
+                   refresh=args.refresh,
+                   retry=args.retry,
+                   expire=args.expire,
+                   negative_ttl=args.negative_ttl,
+                   record_count=args.record_count)
+        for i in xrange(args.zones_count)]
+    master_ns.zones = master_zones
+
+    xfr_zones = [
+        SlaveZone('zone%d.com' % i, master_ips=[master_ns.ip])
+        for i in xrange(args.zones_count)]
+    for xfr in xfrs:
+        xfr.zones = xfr_zones
+
+    resolver_zones = [
+        SlaveZone('zone%d.com' % i, master_ips=[xfr.ip for xfr in xfrs])
+        for i in xrange(args.zones_count)]
+    for resolver in resolvers:
+        resolver.zones = resolver_zones
+
+    if args.zones_count < 1:
         raise ValueError('At least one zone needs to be configured')
-    if args.records_count < 1:
+    if args.record_count < 1:
         raise ValueError('At least one test record needs to be configured')
 
-    loader = jinja2.FileSystemLoader('templates')
-    env = jinja2.Environment(loader=loader)
     clean_existing_directories()
-    build_all_chroots(env, args)
+    for ns in [master_ns] + xfrs + resolvers:
+        ns.build_chroot()
+
     if args.ns_path is not None:
         kill_running_nameservers()
         time.sleep(5)  # TODO: check that they actually stopped
@@ -191,7 +252,8 @@ def main():
         start_nameservers(args.ns_path, not args.no_chroots)
 
     if args.nsupdate_path is not None:
-        nsupdate_loop(env, args)
+        nsupdate_loop(args.nsupdate_path, args.nsupdate_interval,
+                      master_ns, master_zones)
 
 
 if __name__ == '__main__':
